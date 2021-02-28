ad2ee3211d7cd479401a00823de34a5648f99a40
nagi125 <nagi125.dev@gmail.com>
2021-02-28 23:14:45 +0900

    関数を返す関数のサンプル追加

--- src/study1/lessen5/main.go
+++ src/study1/lessen5/main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
-// 無名関数
+func ReturnFunc() func() {
+	return func() {
+		fmt.Println("I'm a function")
+	}
+}
+
 func main() {
+	// 無名関数
 	f := func(x int, y int) int {
 		return x + y
 	}
 
 	i := f(1, 2)
 	fmt.Println(i)
+
+
+	// 関数を返す関数
+	f2 := ReturnFunc()
+	f2()
+
 }
