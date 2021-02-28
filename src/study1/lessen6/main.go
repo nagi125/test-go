67cf46d3fa5e4a818cc2fcb9734bcaf7ce51c75a
nagi125 <nagi125.dev@gmail.com>
2021-02-28 23:56:31 +0900

    for文サンプル追加

--- src/study1/lessen6/main.go
+++ src/study1/lessen6/main.go
@@ -16,4 +16,57 @@ func main() {
 	}
 
 	fmt.Printf("i = %T\n", i)
+
+
+	// 繰り返し処理
+	/*
+	point := 0
+	for point < 10 {
+		fmt.Println("Loop")
+		point++
+	}
+	*/
+
+	// よくあるfor文
+	/*
+	for i := 0; i < 10; i++ {
+		if i == 3 {
+			continue
+		}
+
+		if i == 6 {
+			break
+		}
+		fmt.Println(i)
+	}
+	*/
+
+	// 配列を使ったfor
+	/*
+	arr := [3]int{1, 2, 3}
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+	*/
+
+	// Rangeを使ったfor
+	/*
+	arr := [3]int{1, 2, 3}
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+
+	sl := []string{"Python", "PHP", "Go"}
+	for i, v := range sl {
+		fmt.Println(i, v)
+	}
+	*/
+
+	// 連想配列 for
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
 }
