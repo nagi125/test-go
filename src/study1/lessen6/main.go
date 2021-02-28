fd05556183c691510b37f2d97e47a97591492ada
nagi125 <nagi125.dev@gmail.com>
2021-03-01 00:12:41 +0900

    switchサンプル追加

--- src/study1/lessen6/main.go
+++ src/study1/lessen6/main.go
@@ -69,4 +69,17 @@ func main() {
 		fmt.Println(k, v)
 	}
 
+	// Switchサンプル
+	n := 1
+	switch n {
+	case 1, 2:
+		fmt.Println("1 or 2")
+
+	case 3, 4:
+		fmt.Println("3 or 4")
+
+	default:
+		fmt.Println("I don't know")
+	}
+
 }
