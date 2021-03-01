33476a429f853b86a9da638d7c6a2ede05717a8c
nagi125 <nagi125.dev@gmail.com>
2021-03-01 15:21:52 +0900

    マップサンプル追加

--- src/study2/main.go
+++ src/study2/main.go
@@ -65,4 +65,18 @@ func main() {
 	fmt.Println(a)
 	fmt.Println(s == nil)
 
+	// マップサンプル
+	m1 := make(map[int]string)
+	m1[1]  = "US"
+	m1[81] = "Japan"
+	m1[86] = "China"
+	fmt.Println(m1)
+
+	m2 := map[int]string{
+		1: "Taro",
+		2: "Hanako",
+		3: "Jiro",
+	}
+	fmt.Println(m2)
+
 }
